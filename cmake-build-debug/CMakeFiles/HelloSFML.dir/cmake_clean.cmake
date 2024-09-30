a695e1fdfcb72d2f6af03f9789c3c5790cd9fc91
colinwang2007 <93020461+colinwang2007@users.noreply.github.com>
2024-09-30 15:46:03 -0700

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/HelloSFML.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloSFML.dir/Ball.cpp.o"
+  "CMakeFiles/HelloSFML.dir/Bat.cpp.o"
+  "CMakeFiles/HelloSFML.dir/main.cpp.o"
+  "HelloSFML"
+  "HelloSFML.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloSFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
